b39e2e2b0c9e038fcfed653a667ded02227655ed
Kevin Saanen <kevin@saanen.me>
2023-07-07 12:51:47 +0200

    Initial commit
    Setup project

---
+++ OAuth2.OpenID.API/AuthorizationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace OAuth2.OpenID.API;
+
+public class AuthorizationRequest
+{
+    public String ResponseType { get; set; }
+    public String ClientID { get; set; }
+    public String RedirectURI { get; set; }
+    public String Scope { get; set; }
+    public String State { get; set; }
+    public String Nonce { get; set; }
+}
---
+++ OAuth2.OpenID.API/Controllers/AuthorizeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OAuth2.OpenID.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthorizeController: ControllerBase
+{
+    private readonly ILogger<AuthorizeController> _logger;
+    
+    public AuthorizeController(ILogger<AuthorizeController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet(Name = "Authenticate")]
+    public AuthorizationRequest Get()
+    {
+        return new AuthorizationRequest
+        {
+            Scope = "",
+            Nonce = "",
+            State = "",
+            ResponseType = "",
+            ClientID = "",
+            RedirectURI = ""
+        };
+    }
+}
